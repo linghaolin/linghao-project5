cee87d616007fe16a0d383f43c131bfeef8f69df
Linghao Lin <linlinghao@gmail.com>
2019-03-02 17:54:03 -0500

    Updates

---
+++ precache-manifest.99f1b2279ac0db3dd6040fe62b3f98ef.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "7fd417e2c57c81e6046c",
+    "url": "/stackoverflow-flashcards/static/css/main.bd30f174.chunk.css"
+  },
+  {
+    "revision": "7fd417e2c57c81e6046c",
+    "url": "/stackoverflow-flashcards/static/js/main.7fd417e2.chunk.js"
+  },
+  {
+    "revision": "2f85e1c6e29433cd85be",
+    "url": "/stackoverflow-flashcards/static/js/runtime~main.2f85e1c6.js"
+  },
+  {
+    "revision": "3f605a16a92992ac0f52",
+    "url": "/stackoverflow-flashcards/static/css/2.96d724e2.chunk.css"
+  },
+  {
+    "revision": "3f605a16a92992ac0f52",
+    "url": "/stackoverflow-flashcards/static/js/2.3f605a16.chunk.js"
+  },
+  {
+    "revision": "0e39169bcf020d48f3977a35ce9609e6",
+    "url": "/stackoverflow-flashcards/static/media/userInputBackground.0e39169b.jpg"
+  },
+  {
+    "revision": "8eea7e8d394d484d9ae42074eb2408c4",
+    "url": "/stackoverflow-flashcards/static/media/backgroundImage.8eea7e8d.jpeg"
+  },
+  {
+    "revision": "1feb15a7010e728fb4dff5e7dc14eb9e",
+    "url": "/stackoverflow-flashcards/static/media/cardDeckBackground.1feb15a7.jpg"
+  },
+  {
+    "revision": "fbdbdd128251fe8cad9ae833260c93c3",
+    "url": "/stackoverflow-flashcards/index.html"
+  }
+];
